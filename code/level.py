24a6358ae6f6ff8edff2900907788d5d3009d5b5
Pavel Apunevich <apuniewicz@student.agh.edu.pl>
2023-01-07 19:27:32 +0100

    miś teraz skacze

--- code/level.py
+++ code/level.py
@@ -2,6 +2,7 @@ import pygame
 from player import Player
 from ground import Ground
 
+
 class Level:  # klasa poziomu
     def __init__(self):
         self.display_surface = pygame.display.get_surface()  # pobranie powierzchni ekranu
@@ -11,8 +12,9 @@ class Level:  # klasa poziomu
         self.setup()
 
     def setup(self):
-        self.player = Player((640, 360), self.all_sprites)
         self.ground = Ground((640, 620), self.all_sprites)
+        self.player = Player((160, 480), self.all_sprites)
+
     def run(self, dt):  # funkcja odpowiadająca za działanie poziomu
         self.display_surface.fill('blue')
         self.all_sprites.draw(self.display_surface)
--- code/player.py
+++ code/player.py
@@ -6,44 +6,51 @@ class Player(pygame.sprite.Sprite):
         super().__init__(group)
         # setup
         self.image = pygame.image.load("../sprites/bear.png")
+        size = self.image.get_size()
+        size = (size[0] * 0.8, size[1] * 0.8)
+        self.image = pygame.transform.scale(self.image, size)
         self.image.set_colorkey((255, 255, 255))
         # self.image.fill('green')
         self.rect = self.image.get_rect(center=pos)
 
         self.direction = pygame.math.Vector2()
         self.pos = pygame.math.Vector2(self.rect.center)  # pozycja gracza
-        self.speed = 200
+        self.speed = 1200
+        self.jump = False
 
     def import_assets(self):
         self.animations = {'up': [], 'down': [], 'left': [], 'right': []}  # tworzenie słownika animacji
 
     def input(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP]:
-            self.direction.y = -1
-        elif keys[pygame.K_DOWN]:
-            self.direction.y = 1
-        else:
-            self.direction.y = 0
-
-        if keys[pygame.K_RIGHT]:
-            self.direction.x = 1
-        elif keys[pygame.K_LEFT]:
-            self.direction.x = -1
-        else:
-            self.direction.x = 0
+        if keys[pygame.K_UP] or keys[pygame.K_SPACE]:
+            # self.direction.y = -1
+            self.jump = True
+        # else:
+        #     self.direction.y = 0
 
     def move(self, dt):
 
         if self.direction.magnitude() > 0:  # jeśli długość wektora jest większa od 0
             self.direction = self.direction.normalize()
         # horizontal movement
-        self.pos.x += self.direction.x * self.speed * \
-            dt  # przesunięcie gracza w poziomie
+        self.pos.x += self.direction.x * self.speed * dt
         self.rect.centerx = self.pos.x
         # vertical movement
-        self.pos.y += self.direction.y * self.speed * dt  # przesunięcie gracza w pionie
+        self.pos.y += self.direction.y * self.speed * dt
         self.rect.centery = self.pos.y
+
+        if self.jump is True:
+            self.direction.y = -1
+            self.speed -= 1200 / 100
+        if self.speed == 0:
+            self.direction.y = 1
+        elif self.speed == -1200:
+            self.jump = False
+            self.direction.y = 0
+            self.speed = 1200
+
+        print(self.speed, self.direction.y)
 
     def update(self, dt):  # funkcja aktualizująca stan obiektu
         self.input()
