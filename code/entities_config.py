fdfb4c0ed1827d90969968b972e87f998f60aebe
Pavel Apunevich <apuniewicz@student.agh.edu.pl>
2023-01-14 00:09:06 +0100

    nie wiem co tu pisać już

--- code/entities_config.py
+++ code/entities_config.py
@@ -4,4 +4,4 @@ class Bee():
         self.colorkey = (255, 255, 255)
         self.scale = 0.7
         self.speed = 500
-        self.rect_inflate = (-10, -10)
+        self.rect_inflate = (-10, -15)
