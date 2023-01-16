3e9f77f609cc76b44018d4c3c03c45d8b30e95fc
Pavel Apunevich <apuniewicz@student.agh.edu.pl>
2023-01-16 15:43:19 +0100

    modified: level.py

--- code/level.py
+++ code/level.py
@@ -20,7 +20,8 @@ class Level:  # klasa poziomu
         self.score = Score((SCREEN_WIDTH / 2, 50), self.all_sprites)
         self.player = Player((160, 480), self.all_sprites, PlayerEntity())
         self.bee = Entity((1400, 480), self.all_sprites, Bee())
-        self.honeypot = Entity((1400, 330 * (randint(1, 10)) / 6), self.all_sprites, Honeypot())
+        self.honeypot = Entity((1400, 330 * (randint(1, 10)) / 6),
+                               self.all_sprites, Honeypot())
         self.gameoverEvent = pygame.event.Event(GAMEOVER, NONEDICT)
 
     def run(self, dt):  # funkcja odpowiadająca za działanie poziomu
@@ -30,7 +31,8 @@ class Level:  # klasa poziomu
         if diceroll == 1 or diceroll == 3:
             self.bee = Entity((1400, 480), self.all_sprites, Bee())
         elif diceroll == 2:
-            self.honeypot = Entity((1400, 330 * (randint(1, 50)) / 50), self.all_sprites, Honeypot())
+            self.honeypot = Entity((1400, 330 * (randint(1, 50)) / 50),
+                                   self.all_sprites, Honeypot())
         self.all_sprites.draw(self.display_surface)
         self.all_sprites.update(dt)
         if self.bee.collide(self.player.rect):
