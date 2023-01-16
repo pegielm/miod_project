b5b70879c58cc3b8a5680abf33e04753fc90d4ad
Pavel Apunevich <apuniewicz@student.agh.edu.pl>
2023-01-16 14:57:46 +0100

    honeypot branch

--- code/entities_config.py
+++ code/entities_config.py
@@ -27,3 +27,12 @@ class Bee():
         self.scale = 0.7
         self.speed = 5
         self.rect_inflate = (-10, -15)
+
+
+class Honeypot():
+    def __init__(self):
+        self.sprite = SPRITE_PATH + "honeypot.png"
+        self.colorkey = (255, 255, 255)
+        self.scale = 1
+        self.speed = 0
+        self.rect_inflate = (0, 0)
